ce640e5e1d2205f9ebdc1304c18e78504721a013
José Iván Salazar <josesala@espol.edu.ec>
2026-07-06 19:08:31 +0000

    despliegue

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Grid } from '@mui/material';
 
-  return (        <div>
-            <h1>Bienvenido al Dashboard</h1>
-        </div>
-  )
+function App() {
+   const [count, setCount] = useState(0)
+
+   return (
+      <Grid container spacing={5} sx={{ justifyContent: "left", alignItems: "center" }}>
+
+         {/* Encabezado */}
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+
+         {/* Alertas */}
+         <Grid size={{ xs: 12, md: 3 }}>Elemento: Alertas</Grid>
+
+         {/* Selector */}
+         <Grid size={{ xs: 12, md: 9 }}>Elemento: Selector</Grid>
+
+         {/* Indicadores */}
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Indicadores</Grid>
+
+         {/* Gráfico */}
+         <Grid sx={{ display: { xs: "none", md: "block"}} }>Elemento: Gráfico</Grid>
+
+         {/* Tabla */}
+         <Grid sx={{ display: { xs: "none", md: "block"}}}>Elemento: Tabla</Grid>
+
+         {/* Información adicional */}
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+      </Grid>
+   );
 }
 
-export default App
+export default App;
